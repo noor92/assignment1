40cc3094dc6a15f0274ace1ce4e19e6f590c1438
Noor_Fatima <noor31fat10@gmail.com>
2023-07-22 12:23:00 +0500

    Assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+Install Node.js, TypeScript and VS Code on your computer. Answer Installed and working on it.
+*/
+console.log("Question 1 Answer");
+console.log("Already Installed");
+console.log();
+/*
+Personal Message: Store a person’s name in a variable, and print a message to that person. Your
+message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+*/
+console.log("Question 2 Answer");
+let personName = "Eric";
+let personMessage = '"Hello ' + personName + ', would you like to learn some Python today?"';
+console.log(personMessage);
+console.log();
